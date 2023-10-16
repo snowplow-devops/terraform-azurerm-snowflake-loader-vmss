036fe6140ee56dc3c61e6701f5240ae13ee06de3
jbeemster <joshua.a.beemster@gmail.com>
2023-10-16 16:48:21 +0800

    Prepare for release

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
   module_name_suffix = var.kafka_source == "azure_event_hubs" ? "" : ":${var.kafka_source}"
 
   module_name    = "snowflake-loader-vmss${local.module_name_suffix}"
-  module_version = "0.2.0"
+  module_version = "0.2.1"
 
   app_name = "rdb-loader-snowflake"
 
